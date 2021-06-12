9a84bab6ca2894d226ccb3f8f6598b1b3ea2021b
yassine <chaib.yassine1@gmail.com>
2021-06-12 12:45:21 +0100

    first commit

---
+++ src/test/java/com/example/eurika_discovery/EurikaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurika_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
