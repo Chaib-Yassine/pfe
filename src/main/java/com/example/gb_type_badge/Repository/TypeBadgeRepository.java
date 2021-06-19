edfdb7c8d9f9777a5f054868d76eaaae23dd825c
yassine <chaib.yassine1@gmail.com>
2021-06-19 11:52:10 +0100

    My 3

--- src/main/java/com/example/gb_type_badge/Repository/TypeBadgeRepository.java
+++ src/main/java/com/example/gb_type_badge/Repository/TypeBadgeRepository.java
@@ -4,8 +4,10 @@ import com.example.gb_type_badge.entites.TypeBadge;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin("*")
 public interface TypeBadgeRepository extends JpaRepository<TypeBadge,Long> {
 
 
