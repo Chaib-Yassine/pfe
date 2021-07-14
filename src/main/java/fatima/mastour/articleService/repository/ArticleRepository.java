5bb9562bb619bd6f9fb0fdfcd05bcc046c4ca870
yassine <chaib.yassine1@gmail.com>
2021-07-14 18:45:25 +0100

    3

--- src/main/java/fatima/mastour/articleService/repository/ArticleRepository.java
+++ src/main/java/fatima/mastour/articleService/repository/ArticleRepository.java
@@ -1,9 +1,17 @@
 package fatima.mastour.articleService.repository;
 
 import fatima.mastour.articleService.entities.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin("*")
 public interface ArticleRepository extends JpaRepository < Article, Long >{
+    @RestResource(path = "/byDesignation")
+    public Page<Article> findArticleByDesignationContains(@Param("mc")String titre, Pageable pageable);
 }
